fixed time scaling in the time series subplots.

bin/plot_spd.py
@@ -76,7 +76,7 @@ def plot(spdfile, singlepulsefiles=None, spec_width=1.5, loc_pulse=0.5, xwin=Fal
     start = topo_start - loc_pulse*duration
     datastart = spdobj.waterfall_start_time
     datasamp = spdobj.waterfall_tsamp
-    datanumspectra = spdobj.waterfall_prededisp_nbins
+    datanumspectra = spdobj.waterfall_nbins
     min_freq = spdobj.min_freq
     max_freq = spdobj.max_freq
     sweep_duration = spdobj.sweep_duration
@@ -117,7 +117,7 @@ def plot(spdfile, singlepulsefiles=None, spec_width=1.5, loc_pulse=0.5, xwin=Fal
         
          #### Plot Dedispersed Time series - Zerodm filter - Off
         Dedisp_ts = array[::-1].sum(axis = 0)
-        times = np.arange(datanumspectra)*datasamp
+        times = np.arange(int((datastart-start)/datasamp),int((datastart-start)/datasamp)+datanumspectra)*datasamp
         if integrate_ts: 
             sp_pgplot.ppgplot.pgsvp(0.07, 0.40, 0.80, 0.90)
             sp_pgplot.ppgplot.pgswin(datastart - start, datastart-start+duration, np.min(Dedisp_ts), 1.05*np.max(Dedisp_ts))
@@ -171,7 +171,7 @@ def plot(spdfile, singlepulsefiles=None, spec_width=1.5, loc_pulse=0.5, xwin=Fal
         sp_pgplot.plot_waterfall(array,rangex = [datastart-start, datastart-start+datanumspectra*datasamp],rangey = [min_freq, max_freq],image = 'apjgrey')
         #### Plot Dedispersed Time series - Zerodm filter - On
         dedisp_ts = array[::-1].sum(axis = 0)
-        times = np.arange(datanumspectra)*datasamp
+        times = np.arange(int((datastart-start)/datasamp),int((datastart-start)/datasamp)+datanumspectra)*datasamp
         if integrate_ts:
             sp_pgplot.ppgplot.pgsvp(0.07, 0.40, 0.40, 0.50)
             sp_pgplot.ppgplot.pgswin(datastart - start, datastart-start+duration, np.min(dedisp_ts), 1.05*np.max(dedisp_ts))
@@ -341,7 +341,7 @@ def plot(spdfile, singlepulsefiles=None, spec_width=1.5, loc_pulse=0.5, xwin=Fal
          
         #### Plot Dedispersed Time series - Zerodm filter - Off
         Dedisp_ts = array[::-1].sum(axis = 0)
-        times = np.arange(datanumspectra)*datasamp
+        times = np.arange(int((datastart-start)/datasamp),int((datastart-start)/datasamp)+datanumspectra)*datasamp
         if integrate_ts:
             sp_pgplot.ppgplot.pgsvp(0.1, 0.70, 0.75, 0.83)
             sp_pgplot.ppgplot.pgswin(datastart - start, datastart-start+duration, np.min(Dedisp_ts), 1.05*np.max(Dedisp_ts))
@@ -394,7 +394,7 @@ def plot(spdfile, singlepulsefiles=None, spec_width=1.5, loc_pulse=0.5, xwin=Fal
         
         #### Plot Dedispersed Time series - Zerodm filter - On
         dedisp_ts = array[::-1].sum(axis = 0)
-        times = np.arange(datanumspectra)*datasamp
+        times = np.arange(int((datastart-start)/datasamp),int((datastart-start)/datasamp)+datanumspectra)*datasamp
         if integrate_ts:
             sp_pgplot.ppgplot.pgsvp(0.1, 0.7, 0.36, 0.44)
             sp_pgplot.ppgplot.pgswin(datastart - start, datastart-start+duration, np.min(dedisp_ts), 1.05*np.max(dedisp_ts))